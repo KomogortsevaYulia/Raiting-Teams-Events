4cb64ee3db96a6738b4df578bc1600104bc185bf
Yulia Komogortseva <komog-y-v@yandex.ru>
2022-11-13 12:04:30 +0800

    add team entity

--- server/src/teams/entities/team.entity.ts
+++ server/src/teams/entities/team.entity.ts
@@ -1,1 +1,20 @@
-export class Team {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
+
+@Entity()
+export class Team {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column()
+    title: string
+
+    @Column()
+    direction: string
+
+    @Column()
+    image: string
+
+    @Column()
+    creation_date: Date
+
+}
