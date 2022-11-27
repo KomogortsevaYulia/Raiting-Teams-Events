d1cb835a01482fea62a20aef3e9173127c0ca062
Akul900 <akulinaa2002@gmail.com>
2022-11-28 00:04:36 +0800

    Сделал /teams/id/functions и нет таблицы  direction

--- server/src/teams/teams.service.ts
+++ server/src/teams/teams.service.ts
@@ -2,8 +2,28 @@ import { Injectable } from '@nestjs/common';
 import { CreateTeamDto } from './dto/create-team.dto';
 import { UpdateTeamDto } from './dto/update-team.dto';
 
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
+import { UserFunction } from '../users/entities/user_function.entity';
+import { Repository } from 'typeorm';
+import { Function } from '../users/entities/function.entity';
+import { Team } from './entities/team.entity';
+
+
 @Injectable()
 export class TeamsService {
+
+  constructor(
+    @InjectRepository(Team)  // user //,
+    private readonly teamsRepository: Repository<Team>,
+    @InjectRepository(User)  // user //,
+    private readonly usersRepository: Repository<User>,
+    @InjectRepository(UserFunction)
+    private readonly userFunctionsRepository: Repository<UserFunction>,
+    @InjectRepository(Function)
+    private readonly functionsRepository: Repository<Function>,
+  ) { }
+
   create(createTeamDto: CreateTeamDto) {
     return 'This action adds a new team';
   }
@@ -13,7 +33,7 @@ export class TeamsService {
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} team`;
+    return this.teamsRepository.findOneBy({ id: id });
   }
 
   update(id: number, updateTeamDto: UpdateTeamDto) {
@@ -23,4 +43,28 @@ export class TeamsService {
   remove(id: number) {
     return `This action removes a #${id} team`;
   }
+
+
+
+  async  directionsAndUsers() {
+
+    const directionsUsers = await this.teamsRepository
+    .createQueryBuilder("teams")
+    .select("teams.direction")
+    .getMany()
+
+    return directionsUsers
+  }
+
+  async teamsFunctions(id: number) {
+    //начинаем с функций пользователя
+    const teamsFunctions = await this.functionsRepository
+    .createQueryBuilder("functions")
+    .select("functions.title")
+    .where("functions.team_id = :id", { id: id })
+    .getMany()
+
+    return teamsFunctions
+  }
+
 }
