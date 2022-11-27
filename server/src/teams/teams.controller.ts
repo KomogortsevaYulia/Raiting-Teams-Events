e5a668316161baa2a3820c5cd6e9df02d71aee33
TasyaKh <tasya132002@gmail.com>
2022-11-27 10:20:06 +0800

    создать комнтроллеры

--- server/src/teams/teams.controller.ts
+++ server/src/teams/teams.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpStatus } from '@nestjs/common';
 import { TeamsService } from './teams.service';
 import { CreateTeamDto } from './dto/create-team.dto';
 import { UpdateTeamDto } from './dto/update-team.dto';
-import { ApiTags } from "@nestjs/swagger";
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from "@nestjs/swagger";
+import { Team } from './entities/team.entity';
+import { User } from '../users/entities/user.entity';
+import { UserFunction } from '../users/entities/user_function.entity';
+
 
 @ApiTags('teams')  // <---- Отдельная секция в Swagger для всех методов контроллера
 @Controller('teams')
@@ -33,4 +37,65 @@ export class TeamsController {
   remove(@Param('id') id: string) {
     return this.teamsService.remove(+id);
   }
+
+
+  @Get(':id')
+  @ApiOperation({ summary: "Получение коллектива" })
+  @ApiParam({ name: "id", required: true, description: "Идентификатор коллектива" })
+  @ApiResponse({ status: HttpStatus.OK, description: "Успешно", type: Team })
+  @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: "Bad Request" })
+  findOne(@Param('id') id: number) {
+    return this.teamsService.findOne(id);
+  }
+  
+//   @Post()
+//   create(@Body() createTeamDto: CreateTeamDto) {
+//     return this.teamsService.create(createTeamDto);
+//   }
+
+
+@Get()
+findAll() {
+  return this.teamsService.findAll();
+}
+
+// @Get('title_role/:title_role')
+// teamsAndUsersByRole(@Param('title_role') title_role: string) {
+//   return this.teamsService.teamsWithUsersOfSpecificPosition(title_role);
+// }
+
+//   @Get(':id')
+//   findOne(@Param('id') id: string) {
+//     return this.teamsService.findOne(+id);
+//   }
+
+//   @Patch(':id')
+//   update(@Param('id') id: string, @Body() updateTeamDto: UpdateTeamDto) {
+//     return this.teamsService.update(+id, updateTeamDto);
+//   }
+
+//   @Delete(':id')
+//   remove(@Param('id') id: string) {
+//     return this.teamsService.remove(+id);
+//   }
+
+ //find teams with specific position user
+ @Get('user_role/:title_role')
+ @ApiOperation({ summary: "Получение коллективов с определенной должностью пользователя" })
+ @ApiParam({ name: "title_role", required: true, description: "Идентификатор роли" })
+ @ApiResponse({ status: HttpStatus.OK, description: "Успешно", type: UserFunction })
+ @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: "Bad Request" })
+ teamsWithUsersOfSpecificPosition(@Param('title_role') title_role: string) {
+   return this.teamsService.teamsWithUsersOfSpecificPosition(title_role)
+ }
+
+ //по ид команды найти всех юзеров
+ @Get('with_users/:id')
+ @ApiOperation({ summary: "Получение пользователей по id коллектива" })
+ @ApiParam({ name: "id", required: true, description: "Идентификатор коллектива" })
+ @ApiResponse({ status: HttpStatus.OK, description: "Успешно", type: UserFunction })
+ @ApiResponse({ status: HttpStatus.BAD_REQUEST, description: "Bad Request" })
+ teamsAndUsers(@Param('id') id: number) {
+   return this.teamsService.teamsAndUsers(id)
+ }
 }
