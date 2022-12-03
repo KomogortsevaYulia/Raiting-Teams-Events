a62217cd8c6dc1686dc705a5297c663d23d0dd0a
TasyaKh <tasya132002@gmail.com>
2022-12-03 10:44:25 +0800

    78: добавить к серверу подключение

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,5 +10,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    proxy: {
+      '/api': "http://localhost:3000"
+    }
   }
 })
