7a8eddbf20817be94d76774c7bb8ab1b0a049bac
Yulia Komogortseva <komog-y-v@yandex.ru>
2022-12-04 20:08:52 +0800

    fix

--- server/src/teams/teams.service.ts
+++ server/src/teams/teams.service.ts
@@ -90,7 +90,6 @@ export class TeamsService {
     //начинаем с функций пользователя
     const teamsFunctions = await this.functionsRepository
     .createQueryBuilder("functions")
-    .select("functions.title")
     .innerJoin("functions.team", "team")
     .addSelect("team.title")
     .where("functions.team_id = :id", { id: id })
