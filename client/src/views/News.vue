38128104d3518f36f940b5241a4255ffb065ed88
Maria <msadovnikov904@gmail.com>
2022-12-04 21:11:00 +0800

    Карточки новостей

--- client/src/views/News.vue
+++ client/src/views/News.vue
@@ -2,24 +2,53 @@
   <div>
       <div className = "mainBanner">
         <span className = "descr"> 
-          Описание проекта
-          <button className = "bannerBtn">Нажми меня</button>
+        Надо что-то написать 
+        <button className = "bannerBtn">Нажми меня</button>
         </span>
       </div>
-      <div className = "container">
+      <div className = "cardContainer">
         <div class="cardEvent">
-            <img src="../assets/icon/event1.png" className = "imgEvent">            
-            <div class="wrapper-content">
+            <div className = "imgEvent">
+              <div></div>
+              <p>Региональный чемпионат WorldSkills “Инженерный дизайн CAD”</p>  
+              <img src="../assets/icon/event1.png">
+            </div>
+            <div class="wrapperContent">
               <div>
-                <a>Клуб "Автостопом по галактике"</a>
-                <p>Мы любители - звездочеты, ищем себе в клуб ребят, которые горят желанием изучать космос, серьезно
-                  заниматься астрономией и просто любоваться на звездное небо!</p>
-                <p>Руководитель: Иванов Иван Иванович</p>
+                <p >
+                  Проводится с целью выработки у студентов навыков работы в области конструирования, для развития конструкторских навыков, творческого потенциала в решении сложных нестандартных задач за короткий промежуток времени
+                </p>
+                <p className = "date">01.04.2021</p>
               </div>
-              <div class="btn">
-                <RouterLink to="/team-1">
-                  <button>Подробнее</button>
-                </RouterLink>
+            </div>
+          </div>
+          <div class="cardEvent">
+            <div className = "imgEvent">
+              <div></div>
+              <p>Викторина “Роль и место прокуратуры России в Российской Федерации”</p>  
+              <img src="../assets/icon/event3.png">
+            </div>
+            <div class="wrapperContent">
+              <div>
+                <p>
+                  Викторина для школьников “Роль и место прокуратуры России в Российской Федерации”
+                </p>
+                <p className = "date">05.04.2021</p>
+              </div>
+            </div>
+          </div>
+          <div class="cardEvent">
+            <div className = "imgEvent">
+              <div></div>
+              <p>Конкурс “Защити свою профессию” по профилю “Электрические станции”</p>  
+              <img src="../assets/icon/event2.png">
+            </div>
+            <div class="wrapperContent">
+              <div>
+                <p>
+                  В конкурсе принимают участие студенты 4 курса профиля “Электрические станции”. Цель конкурса: выявить лучших студентов по уровню освоения профессиональных компетенций и способностей к творческому мышлению
+                </p>
+                <p className = "date">06.04.2021</p>
               </div>
             </div>
           </div>
