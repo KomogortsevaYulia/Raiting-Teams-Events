ec12f5c43fbe096d5125e5c68cddbb049976606a
TasyaKh <tasya132002@gmail.com>
2022-12-09 23:44:43 +0800

    82: дтошки с проверками значений, библиотека class-validator

--- server/src/teams/dto/create-team.dto.ts
+++ server/src/teams/dto/create-team.dto.ts
@@ -6,7 +6,7 @@ export class CreateTeamDto {
 
     @IsNotEmpty({ message: 'Поле пустое' })
     @Length(1,50,{
-        message: 'Максимальная длина текста 50'
+        message: 'Название, максимальная длина текста 50'
     })
     title: string
 
@@ -16,7 +16,7 @@ export class CreateTeamDto {
     image: string
 
     @Length(1,1000,{
-        message: 'Максимальная длина текста 1000'
+        message: 'Описание, максимальная длина текста 1000'
     })
     description: string
 
