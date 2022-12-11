6d32d467aff9087b11e9fe3a335f2fa98f33cc01
Maria <msadovnikov904@gmail.com>
2022-12-11 17:43:47 +0800

    ..

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -76,5 +76,74 @@
 </template>
 
 <style lang="scss">
-    @import '@/assets/login.scss';
+.form-login {
+    display: flex;
+    padding: 2rem 4rem 2rem 4rem;  
+    border-radius: 20px;
+    height: 500px auto;
+    width: 30%;
+    flex-direction: column;
+    // box-shadow: rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgb(209, 213, 219) 0px 0px 0px 1px inset;
+    border: var(--main-border-card);
+
+    a {
+        font-size: 25px;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+        text-align: center;
+        padding: 0 0 4rem 0;
+    }
+
+    .form-login__choice {
+        display: flex;
+        justify-content: center;
+        padding-bottom: 1rem;
+
+        a {
+            cursor: pointer;
+            font-size: 14px;
+            padding: 0 0.75rem 0.75rem 0.75rem;
+
+            &:hover {
+                color: var(--main-color);
+            }
+
+            &:active {
+                transition: all 0.2s;
+                color: var(--main-color-hover);
+                border-bottom-color: var(--main-color-hover);
+            }
+        }
+
+        .active {
+            border-bottom: var(--main-border-bottom);
+        }
+    }
+
+    .form-login__input {
+        display: flex;
+        flex-direction: column;
+        padding: 0.75rem 0 0.75rem 0;
+    }
+
+    .form-login__submit-campus {
+        display: flex;
+        justify-content: center;
+    }
+
+    .form-login__submit {
+        display: flex;
+        justify-content: end;
+
+        p {
+            cursor: pointer;
+            padding-right: 1rem;
+            font-size: 14px;
+            font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+            &:hover {
+                color: #4c70ff;
+            }
+        }
+    }
+}
 </style>
