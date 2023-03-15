8c090521541aea9d9c745be8aaa9b1680101fbf2
KutenkovV <kutenkovvlad017@gmail.com>
2023-03-15 12:20:15 +0800

    +

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,10 @@ import { createApp } from "vue";
 import { createPinia } from 'pinia';
 import App from "./App.vue";
 import router from "./router";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
 // import store from "./store";
 
 import "./assets/style.scss";
