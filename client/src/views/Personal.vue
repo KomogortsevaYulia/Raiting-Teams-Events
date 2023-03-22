0497421d8a9822b36aab27767801773ee80289d1
poopitscwish <shenya200lalala@mail.ru>
2023-03-22 12:11:36 +0800

    Починил кнопки календаря

--- client/src/views/Personal.vue
+++ client/src/views/Personal.vue
@@ -19,7 +19,7 @@
     </div>
   </div>
   <div>
-    <VCalendar :rows="2" />
+    <VCalendar />
   </div>
 
 </template>
@@ -27,17 +27,13 @@
 .container {
   display: flex;
 }
-.vc-blue {
-  --vc-accent-50: #f0f9ff;
-  --vc-accent-100: #e0f2fe;
-  --vc-accent-200: #bae6fd;
-  --vc-accent-300: #7dd3fc;
-  --vc-accent-400: #38bdf8;
-  --vc-accent-500: #0ea5e9;
-  --vc-accent-600: #0284c7;
-  --vc-accent-700: #0369a1;
-  --vc-accent-800: #075985;
-  --vc-accent-900: #0c4a6e;
+.vc-arrow{
+  width: 35px;
+  height: 30px;
+  color: #fd524c;
+}
+button{
+  padding: 0em;
 }
 .avatar {
   margin-right: 2em;
