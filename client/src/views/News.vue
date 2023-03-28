bf7a91e434d632913c63e081d2556847690deee9
Maria <msadovnikov904@gmail.com>
2023-03-28 13:22:16 +0800

    Страница мероприятий под Vue3 синтаксис

--- client/src/views/News.vue
+++ client/src/views/News.vue
@@ -17,9 +17,9 @@
             <span class="span__title">{{ menu_type.title }}</span>
           </label>    
           <div class="btn__open" v-if="menu_item.menu_types.length > 4" @click="menu_item.hidden = !menu_item.hidden">
-            <div class="btn__text" v-if="hiddenCheckboxStatus">Развернуть</div>
+            <div class="btn__text" v-if="menu_item.hidden">Развернуть</div>
             <div v-else class="btn__text">Свернуть</div>
-            <div class="btn__img" :class="{'closed': hiddenCheckboxStatus}"></div>
+            <div class="btn__img" :class="{'closed': menu_item.hidden}"></div>
           </div>
         </div>
       </div>
@@ -54,58 +54,15 @@
   </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import Switch_toggle from '@/components/Switch_toggle.vue';
+import { useEventStore } from "@/store/events_store";
 
-export default{
-  components:{
-    Switch_toggle
-  },
-  data(){
-    return{
-      menu_items: [
-        {id: 1, title: 'Формат проведения', menu_types:[
-          {id: 1, title:'Online'},
-          {id: 2, title:'Offline'},
-        ]},
-        {id: 2, title: 'Уровень', menu_types:[
-          {id: 1, title:'Внутривузовский'},
-          {id: 2, title:'Межвузовский'},
-          {id: 3, title:'Региональный'},
-          {id: 4, title:'Всероссийский'},
-        ]},
-        {id: 3, title: 'Институт', hidden: true, menu_types:[
-          {id: 1, title:'Авиамашиностроения и транспорта'},
-          {id: 2, title:'Архитектуры, строительства и дизайна'},
-          {id: 3, title:'Высоких технологий'},
-          {id: 4, title:'Информационных технологий и анализа даных'},
-          {id: 5, title:'Квантовой физики'},
-          {id: 6, title:'Лингвистики и межкультурной коммуникации'},
-          {id: 7, title:'Недропользования'},
-          {id: 8, title:'Экономики, управления и права'},
-          {id: 9, title:'Энергетики'},
-          {id: 10, title:'БРИКС'},
-        ]},
-        {id: 4, title: 'Курс', hidden: true, menu_types:[
-          {id: 1, title:'1 курс'},
-          {id: 2, title:'2 курс'},
-          {id: 3, title:'3 курс'},
-          {id: 4, title:'4 курс'},
-          {id: 5, title:'5 курс'},
-          {id: 6, title:'Магистратура'},
-        ]}
-      ],
-      hiddenCheckboxStatus: true
-    }
-  },
-  methods:{
-  
-  }
-}
+const eventStore = useEventStore();
+const menu_items = eventStore.menu_items;
 </script>
 
 <style lang="scss" scoped>
-
 .events__container{
   display: flex;
   padding-top: 1rem;
