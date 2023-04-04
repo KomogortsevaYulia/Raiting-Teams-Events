b6aa29121a30b68ea3909f1e44d97b8ae6863fab
Yulia Komogortseva <komog-y-v@yandex.ru>
2023-04-04 14:23:03 +0800

    Обновила бэкап

---
+++ server/src/migrations/1680581129109-auto.ts
@@ -0,0 +1,14 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class auto1680581129109 implements MigrationInterface {
+    name = 'auto1680581129109'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "teams" ADD "short_description" character varying`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "teams" DROP COLUMN "short_description"`);
+    }
+
+}
