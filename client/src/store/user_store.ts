cf1a13b565e04200c3a446c9b0d944400503f9f7
Yulia Komogortseva <komog-y-v@yandex.ru>
2023-04-06 19:46:07 +0800

    делаю ЛК

--- client/src/store/user_store.ts
+++ client/src/store/user_store.ts
@@ -1,24 +1,29 @@
-
 import { defineStore } from "pinia";
 import axios from "axios";
 
 export const useUserStore = defineStore("user", () => {
 
-    async function getUsersByNameEmail(limit:Number, fullname:String, email:String) {
+  async function getUsersByNameEmail(limit: Number, fullname: String, email: String) {
+    return await axios.get("api/users", {
+      params: {
+        limit: limit,
+        fullname: fullname,
+        email: email
+      }
+    });
+  }
 
-        return await axios.get("api/users", {
-          params: {
-            limit: limit,
-            fullname: fullname,
-            email: email
-          }
-        });
-    }
+  async function getInfoUser() {
+    let response = await axios.get("api/users/check-login")
+    if (response){return response}
+    // return await axios.get('api/users/'+username, {
+    // });
+  }
 
-    return {
-        getUsersByNameEmail
-    }
+  return {
+    getUsersByNameEmail,
+    getInfoUser
+  }
 });
 
 
-  
