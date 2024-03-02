268def8f10d7d6954256c37554224f5d534f3650
TasyaKh <AkitaSpam@gmail.com>
2024-03-02 10:34:26 +0800

    feat: add in cabinets (direction and tags)

--- server/src/schedule/entities/cabinets.entity.ts
+++ server/src/schedule/entities/cabinets.entity.ts
@@ -1,7 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
-import { TeamSchedule } from './schedule.entity';
-import { CabinetsTime } from './cabinets-time.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Team } from '../../teams/entities/team.entity';
 
 @Entity('cabinets')
 export class Cabinets {
@@ -12,6 +17,14 @@ export class Cabinets {
   @ApiProperty()
   @Column()
   name: string;
+
+  @ManyToOne(() => Team, (team) => team.id, { onDelete: 'CASCADE' })
+  @JoinColumn([{ name: 'id_direction' }])
+  direction: Team;
+
+  @ApiProperty()
+  @Column('simple-array', {nullable:true})
+  tags: string[]; // теги
 
   // @OneToMany(() => CabinetsTime, (cabinetsTime) => cabinetsTime.cabinet, {
   //   cascade: true,
