894046b577e9ebdae5598342bcb4d0bab79bd91f
AnnaVyvert <0073av@gmail.com>
2024-03-10 19:11:41 +0800

    feat(FE/common): 🌟 month enums

---
+++ client/src/common/date/month.enum.ts
@@ -0,0 +1,29 @@
+export enum Month {
+  JANUARY = "JANUARY",
+  FEBRUARY = "FEBRUARY",
+  MARCH = "MARCH",
+  APRIL = "APRIL",
+  MAY = "MAY",
+  JUNE = "JUNE",
+  JULY = "JULY",
+  AUGUST = "AUGUST",
+  OCTOBER = "OCTOBER",
+  SEPTEMBER = "SEPTEMBER",
+  NOVEMBER = "NOVEMBER",
+  DECEMBER = "DECEMBER",
+}
+
+export enum MonthRU {
+  JANUARY = "Январь",
+  FEBRUARY = "Февраль",
+  MARCH = "Март",
+  APRIL = "Апрель",
+  MAY = "Май",
+  JUNE = "Июнь",
+  JULY = "Июль",
+  AUGUST = "Август",
+  OCTOBER = "Октябрь",
+  SEPTEMBER = "Сентябрь",
+  NOVEMBER = "Ноябрь",
+  DECEMBER = "Декабрь",
+}
